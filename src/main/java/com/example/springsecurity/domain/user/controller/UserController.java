4cff43c09f1413befb43b895c216483144702103
kudongku <kudongku@naver.com>
2024-08-02 15:13:59 +0900

    :sparkles: feat : controller에서 dto 받는 것으로 수정

--- src/main/java/com/example/springsecurity/domain/user/controller/UserController.java
+++ src/main/java/com/example/springsecurity/domain/user/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.example.springsecurity.domain.user.controller;
 
+import com.example.springsecurity.domain.user.dto.UserLoginRequestDto;
 import com.example.springsecurity.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +17,13 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/login")
-    public ResponseEntity<String> login() {
-        return ResponseEntity.ok(userService.login("username", "password"));
+    public ResponseEntity<String> login(@RequestBody UserLoginRequestDto userLoginRequestDto) {
+        String bearerToken = userService.login(
+            userLoginRequestDto.getUsername(),
+            userLoginRequestDto.getPassword()
+        );
+
+        return ResponseEntity.ok(bearerToken);
     }
 
 }
