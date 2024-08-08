364914ab2b93259b3102a467c7336a35adf2b042
kudongku <kudongku@naver.com>
2024-08-08 18:00:18 +0900

    :sparkles: feat : JwtFilter에서 exception 발생시 handle

--- src/main/java/com/example/springsecurity/global/filter/JwtFilter.java
+++ src/main/java/com/example/springsecurity/global/filter/JwtFilter.java
@@ -1,6 +1,9 @@
 package com.example.springsecurity.global.filter;
 
 import com.example.springsecurity.global.util.JwtUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +17,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j(topic = "JwtFilter")
@@ -28,45 +32,67 @@ public class JwtFilter extends OncePerRequestFilter {
         @NonNull HttpServletResponse response,
         @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-
         String path = request.getRequestURI();
 
         // 로그인 및 회원가입 경로에서는 필터를 건너뛰기
-        if (path.equals("/api/v1/users/signup") || path.equals("/api/v1/users/login") || path.equals("/api/v1/users/refreshToken")) {
+        if (path.startsWith("/api/v1/users/")) {
             filterChain.doFilter(request, response);
             return;
         }
 
         String token = jwtUtil.getJwtFromHeader(request);
 
-        if (token == null) {
-            log.error("토큰이 유효하지 않습니다.");
-            filterChain.doFilter(request, response);
+        if (!StringUtils.hasText(token)) {
+            String errorMessage = "토큰이 제공되지 않았습니다.";
+            filterExceptionHandler(response, errorMessage);
             return;
         }
 
-        // token이 만료되었는지 확인하기
-        if (jwtUtil.isExpired(token)) {
-            log.error("토큰이 만료되었습니다.");
-            filterChain.doFilter(request, response);
+        try {
+            String username = jwtUtil.getUsernameFromToken(token);
+            List<SimpleGrantedAuthority> authorities = jwtUtil.getAuthoritiesFromToken(token);
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                username,
+                null,
+                authorities
+            );
+            authenticationToken.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request)
+            );
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        } catch (SecurityException | MalformedJwtException e) {
+            String errorMessage = "Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.";
+            filterExceptionHandler(response, errorMessage);
+            return;
+        } catch (ExpiredJwtException e) {
+            String errorMessage = "Expired JWT token, 만료된 JWT token 입니다.";
+            filterExceptionHandler(response, errorMessage);
+            return;
+        } catch (UnsupportedJwtException e) {
+            String errorMessage = "Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.";
+            filterExceptionHandler(response, errorMessage);
+            return;
+        } catch (IllegalArgumentException e) {
+            String errorMessage = "JWT claims is empty, 잘못된 JWT 토큰 입니다.";
+            filterExceptionHandler(response, errorMessage);
+            return;
+        } catch (Exception e) {
+            filterExceptionHandler(response, e.getMessage());
             return;
         }
 
-        // username token 에서 꺼내기
-        String username = jwtUtil.getUsernameFromToken(token);
-        List<SimpleGrantedAuthority> authorities = jwtUtil.getAuthoritiesFromToken(token);
-
-        // 권한 부여
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-            username,
-            null,
-            authorities
-        );
-
-        // Detail을 넣는 단계
-        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         filterChain.doFilter(request, response);
+
+    }
+
+    private void filterExceptionHandler(
+        HttpServletResponse response,
+        String errorMessage
+    ) throws IOException {
+        log.error(errorMessage);
+        response.setStatus(403);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(errorMessage);
     }
 
 }
